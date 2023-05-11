56d2a5612f62db8c9097df54682a1ebb242bb586
Digvijay Singh <107340811+roblox-codex@users.noreply.github.com>
2023-05-11 23:39:02 +0530

    Create reader_writer.cpp

---
+++ OS_LAB/reader_writer.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <pthread.h>
+#include <semaphore.h>
+using namespace std;
+
+sem_t db; // semaphore
+pthread_mutex_t mutex; // mutex
+int os = 1;
+int crypto = 0;
+
+void* writer(void* wno) {
+    // Entry Section
+    sem_wait(&db); // down(db)
+    // Critical Section
+    os = os * 2;
+    cout << "Writer " << *(static_cast<int*>(wno)) << " modified os to " << os << endl;
+    // Exit Section
+    sem_post(&db); // up(db)
+}
+
+void* reader(void* rno) {
+    // Entry Section
+    pthread_mutex_lock(&mutex); // down(mutex)
+    crypto++;
+    if (crypto == 1)
+        sem_wait(&db); // down(db)
+    pthread_mutex_unlock(&mutex); // up(mutex)
+    // Critical Section
+    cout << "Reader " << *(static_cast<int*>(rno)) << ": read os as " << os << endl;
+    // Exit Section
+    pthread_mutex_lock(&mutex); // down(mutex)
+    crypto--;
+    if (crypto == 0)
+        sem_post(&db); // up(db)
+    pthread_mutex_unlock(&mutex); // up(mutex)
+}
+
+int main() {
+    pthread_t read[10], write[5];
+    pthread_mutex_init(&mutex, nullptr);
+    sem_init(&db, 0, 1);
+    int a[10] = { 1,2,3,4,5,6,7,8,9,10 };
+    for (int i = 0; i < 10; i++) {
+        pthread_create(&read[i], nullptr, reader, &a[i]);
+    }
+    for (int i = 0; i < 5; i++) {
+        pthread_create(&write[i], nullptr, writer, &a[i]);
+    }
+    for (int i = 0; i < 10; i++) {
+        pthread_join(read[i], nullptr);
+    }
+    for (int i = 0; i < 5; i++) {
+        pthread_join(write[i], nullptr);
+    }
+    pthread_mutex_destroy(&mutex);
+    sem_destroy(&db);
+    return 0;
+}
